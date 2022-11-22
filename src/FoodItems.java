cc2d22b479d3628e12398fc700c6f1f8302dc93a
hami-r <mynameissaitama54@gmail.com>
2022-11-22 12:39:17 +0530

    food class added

--- src/FoodItems.java
+++ src/FoodItems.java
@@ -1,20 +1,17 @@
 public class FoodItems {
-    public class FoodItems {
-        int tea = 10;
-        int coffee = 15;
-        int snacks = 10;
-        int idli = 10;
-        int dosa = 6;
+    int tea = 10;
+    int coffee = 15;
+    int snacks = 10;
+    int idli = 10;
+    int dosa = 6;
 
-        public FoodItems() {
-            System.out.println("""
+    public FoodItems() {
+        System.out.println("""
                     --------ITEMS-------------
                     Tea - 10
                     Coffee - 15
                     Snacks - 10
                     Idli - 10
                     Dosa - 6""");
-        }
     }
-
 }
